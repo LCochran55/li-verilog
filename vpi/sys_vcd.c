8dd281f32d6b3df1e21c719ac2e1a30bddf74d57
LCochran55 <liamcochran55@gmail.com>
2026-06-02 16:48:45 -0700

    Typos

--- vpi/sys_vcd.c
+++ vpi/sys_vcd.c
@@ -479,9 +479,9 @@ static void open_dumpfile(vpiHandle callh) {
 	    }
 
 		if (!dump_no_date) {
-			fprintf(dumpfile,"$date\n");
-			fprintf(dumpfile, "\t%s",asctime(localtime(&walltime)));
-			fprintf(dumpfile, "$end\n");
+			fprintf(dump_file,"$date\n");
+			fprintf(dump_file, "\t%s",asctime(localtime(&walltime)));
+			fprintf(dump_file, "$end\n");
 		}
 	    kprintf("$version\n");
 	    kprintf("\tIcarus Verilog\n");
